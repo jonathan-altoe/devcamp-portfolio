16be37b0fadc7b03e94b990e1c2e20495ff7c588
jonathan altoe <jonathan.altoe@coupa.com>
2018-07-13 16:52:32 +0200

    Integrated update and edit + small fix image tag

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,9 +2,11 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
   def new
     @portfolio_item = Portfolio.new
   end
+
   def create
     @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -17,4 +19,19 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 end
