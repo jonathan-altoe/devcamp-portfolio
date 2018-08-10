72f66c06ee92499ed167fe179fe77ee1b6ec4d3d
jonathan altoe <jonathan.altoe@coupa.com>
2018-08-10 16:33:00 +0200

    Built twitter parser for tweet links

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,26 @@
 module PagesHelper
+  def twitter_parser tweet
+
+    regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+
+    tweet.gsub(regex) do |url_patern|
+      "<a href='#{url_patern}' target='_blank'>#{url_patern}</a>"
+    end.html_safe
+  end
 end
