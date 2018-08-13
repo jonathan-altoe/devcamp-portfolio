23da4e18b86b31a6b4e0a1a9f6c7620650f0a767
jonathan altoe <jonathan.altoe@coupa.com>
2018-08-13 14:52:02 +0200

    Implemented custom scope for ordering by created date desc. Also hide hide draft blogs for non admin user

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,11 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    if logged_in?(:site_admin)
+      @blogs = Blog.order_by_most_recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.order_by_most_recent.published.page(params[:page]).per(5)
+    end
     @page_title = 'My Portfolio Blogs'
   end
 
