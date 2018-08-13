5243c630e6e4321422aa1ccd730c03a4d3cb6f4c
jonathan altoe <jonathan.altoe@coupa.com>
2018-08-13 16:09:30 +0200

    Implemented the blog widget for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_sidebar_topics
   layout 'blog'
 
   def index
@@ -8,12 +9,16 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
 
-
     if logged_in?(:site_admin)
       @blogs = @topic.blogs.order_by_most_recent.page(params[:page]).per(5)
     else
       @blogs = @topic.blogs.order_by_most_recent.published.page(params[:page]).per(5)
     end
+  end
 
+  private
+
+  def set_sidebar_topics
+    @side_bar_topics = Topic.with_blogs
   end
 end
